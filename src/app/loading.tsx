3a9a0d884f70eb8c09062eb43047a3209702f1c7
brayansuamon <brayansuamon@gmail.com>
2024-01-29 07:52:07 -0500

    Fixed loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,8 +5,8 @@ export interface ILoadingProps {
 
 export default function Loading (props: ILoadingProps) {
   return (
-    <section className="w-full h-full min-h-[200px]">
-        <Image fill src={'/images/Loading2.avif'} alt={'Loading'} />
+    <section className="mx-auto min-w-1/2 h-full min-h-[200px]">
+        <Image fill src={'/images/Loading2.avif'} alt={'Loading'} className="object-contain"/>
     </section>
   );
 }
